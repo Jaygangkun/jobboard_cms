c5bb8a6c9e3b198c443a852b91e2478b47170e64
JayGangkun <jaygangkun@hotmail.com>
2020-11-03 09:58:04 +0800

    update config of live site

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.16.1.45:8915';
+// $config['base_url'] = 'http://172.16.1.45:8915';
+$config['base_url'] = 'http://teenstreet.tdbapply.com/';
 
 /*
 |--------------------------------------------------------------------------
